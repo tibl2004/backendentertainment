ebd651ccbf1d93364eb363b01715f5b12d3feaad
Timo Blumer <timo.blumer@gewa.ch>
2024-04-30 22:42:13 +0200

    upate

--- routes/videos.router.js
+++ routes/videos.router.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 
-
 const videoController = require("../controller/video.controller");
+
+const upload = multer({ dest: 'uploads/' }); // Hier wird 'upload' definiert
 
 router.get("/", videoController.getAllVideos);
 router.get("/:id", videoController.getVideoById);
 router.post("/", videoController.createVideo);
 router.put("/:id", videoController.updateVideo);
 router.delete("/:id", videoController.deleteVideo);
-router.post("/upload", upload.single('video'), videoController.uploadVideo);
+router.post("/upload", upload.single('video'), videoController.uploadVideo); // Hier wird 'upload' verwendet
 
 module.exports = router;
