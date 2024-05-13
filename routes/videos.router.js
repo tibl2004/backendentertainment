ffa782d01728de5fb72d349952729f5904bc9b5b
Timo Blumer <timo.blumer@gewa.ch>
2024-05-13 13:50:07 +0200

    uparjfjs

--- routes/videos.router.js
+++ routes/videos.router.js
@@ -1,10 +1,9 @@
+// video.router.js
+
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
 
 const videoController = require("../controller/video.controller");
-
-
 
 router.get("/", videoController.getAllVideos);
 router.get("/:id", videoController.getVideoById);
